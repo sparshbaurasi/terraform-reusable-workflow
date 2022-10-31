70aa706d94fc1594d5df868fa29a61280e6edd3c
sparshbaurasi <107546419+sparshbaurasi@users.noreply.github.com>
2022-10-31 18:39:01 +0530

    Update main.tf

--- modules/s3-module/main.tf
+++ modules/s3-module/main.tf
@@ -1,8 +1,3 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
-
-resource "aws_s3_bucket_policy" "s3_bucket_policy" {
-  bucket = aws_s3_bucket.s3_bucket.id
-  policy = var.bucket_policy
-}
